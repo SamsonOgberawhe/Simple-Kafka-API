425c8d89d3927fc588d21c2ff22e2c7e1e78cab5
Sammy19956 <samson.ogberawhe@decagon.dev>
2023-03-10 13:30:23 +0100

    started project

---
+++ src/main/java/com/samson/simple_kafka_api/SimpleKafkaApiApplication.java
@@ -0,0 +1,13 @@
+package com.samson.simple_kafka_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKafkaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleKafkaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samson/simple_kafka_api/SimpleKafkaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samson.simple_kafka_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
